d971cbdcaddc9ad7587a6dd56d6aa193a37c12f5
андрей китенко <genialniy.kitenko@gmail.com>
2021-04-15 12:36:45 +0300

    added comments

--- test.py
+++ test.py
@@ -16,11 +16,11 @@ class DataGenerator:
         self.is_train = is_train
         self.image_shape = image_shape
         self.num_classes = 3
-        print(self.image_shape[1])
 
         # read json
         with open('datasetdata.json') as f:  # открыли файл с данными
             dicti = json.load(f)
+
 
         if is_train:
             self.data = dicti['train']
@@ -28,10 +28,15 @@ class DataGenerator:
             self.data = dicti['test']
 
         # shuffle data
-        l = list(self.data.items())
-        random.shuffle(l)
-        self.data = dict(l)
-        print(len(self.data))
+        """
+        Тут соотвественно перемешиваются наши данные, если сделать print(), то можно увидеть, что перемешалось всё 
+        хорошо. 
+        
+        """
+        data = list(self.data.items())
+        random.shuffle(data)
+        self.data = dict(data)
+        #print(self.data)
 
 
     def __iter__(self):
@@ -48,17 +53,32 @@ class DataGenerator:
 
     def __len__(self):
 
+        """
+        :return:
+        Here I used "math" module to round the "number_of_batch" up.
+        """
+
         self.number_of_batch = math.ceil(len(self.data) / self.batch_size)
         return self.number_of_batch
 
 
     def __getitem__(self, batch_idx: int) -> Tuple[np.ndarray, np.ndarray]:
-        self.exit_batch = np.zeros((16, 224, 224, 3))
+        """
+        :param batch_idx:
+        :return:
+
+        Here we return batch (numpy array(batch_size, image_h, image_w, 3). Also we return "onehot encoding", where there
+        are batch_size and number of classes.
+        """
+
         data = self.data
+        # тут можно не использовтьа sorted? У меня почему-то раюотает только с sordet.
         data_list = sorted(data.items())[(batch_idx * self.batch_size) : ((batch_idx + 1) * self.batch_size)]
+        # после sorted я снова перемешиваю
         data_list = random.sample(data_list, len(data_list))
         self.images = np.zeros((self.batch_size, self.image_shape[0], self.image_shape[1], self.image_shape[2]))
         self.labels = np.zeros((self.batch_size, self.num_classes))
+        # возможно тут начинаются проблемы
         for i, (img_path, class_name) in enumerate(data_list):
             img = cv2.imread(img_path)
             self.resized_image = cv2.resize(img, (self.image_shape[0], self.image_shape[1]))
@@ -72,6 +92,7 @@ class DataGenerator:
             else:
                 self.labels[i, 2] = 1
 
+            print(self.labels)
         return self.images, self.labels
 
 
@@ -99,5 +120,5 @@ class DataGenerator:
 
 x = DataGenerator(json_path='/home/andre/pycharm/python_cv_tasks/task_1', batch_size=16, is_train=False, image_shape=(224, 224, 3))
 
-x.__getitem__(1)
-x.image_grafic()
+#x.__getitem__(2)
+#x.image_grafic()
