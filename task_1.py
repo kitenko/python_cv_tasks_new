fade0d7e36b9f47dae9ebb097900de9552b7648c
андрей китенко <genialniy.kitenko@gmail.com>
2021-04-18 14:10:14 +0300

    Я попытался исправить всё что смог, однако остались моменты которые я не оправил. Которые необходимо с тобой обсудить.

--- task_1.py
+++ task_1.py
@@ -5,7 +5,6 @@ from typing import Tuple
 import numpy as np
 import cv2
 
-
 DATASET_PATH = os.path.join('dataset_image')
 DATASET_PATH_RED = os.path.join(DATASET_PATH, 'red_image')
 DATASET_PATH_BLUE = os.path.join(DATASET_PATH, 'blue_image')
@@ -13,103 +12,81 @@ DATASET_PATH_GREEN = os.path.join(DATASET_PATH, 'green_image')
 ALL_PATH = [DATASET_PATH_RED, DATASET_PATH_GREEN, DATASET_PATH_BLUE]
 
 
-def create_dataset(number_of_images: int, image_wight_range: Tuple[int, int], image_height_range: Tuple[int, int],
-                   colour: str, create_dataset:bool) -> None:
+def create_dataset(number_of_images: int, image_wight_range: Tuple[int, int], image_height_range: Tuple[int, int]) -> None:
     """
     This function create dataset of images (red, green, blue)
 
     :param number_of_images: number of images for one color in folder.
     :param image_wight_range: the wight of the image changes within the specified range.
     :param image_height_range: the height of the image changes within the specified range.
-    :param colour: which color of image you want to create
-    :param create_dataset: create folders or not for images
-    :return: None
+    :param colour: which color of image you want to create.
     """
+    # create folders for images
+    for i in ALL_PATH:
+        os.makedirs(i, exist_ok=True)
 
-    if create_dataset:
-        for i in ALL_PATH:
-            os.makedirs(i, exist_ok=True)
+    # number of images in one folder
+    part_number_of_images = number_of_images // 3
 
-    if colour == 'red':
-        for i in range(number_of_images):
-            # random height and width
-            width = np.random.randint(image_wight_range[0], image_wight_range[1])
-            height = np.random.randint(image_height_range[0], image_height_range[1])
-            # create and save image
-            image_red = np.random.randint(0, 255, (width, height, 1))
-            image_zeros = np.zeros((width, height, 1), np.uint8)
-            final_image = np.concatenate((image_zeros, image_zeros, image_red), axis=-1)
+    for i in range(number_of_images):
+        width = np.random.randint(image_wight_range[0], image_wight_range[1])
+        height = np.random.randint(image_height_range[0], image_height_range[1])
+        # create and save image
+        image = np.random.randint(0, 255, (width, height, 1))
+        image_zeros = np.zeros((width, height, 1), np.uint8)
+        if i < part_number_of_images:
+            final_image = np.concatenate((image_zeros, image_zeros, image), axis=-1)
             cv2.imwrite(os.path.join(DATASET_PATH_RED, 'red_{}.jpg'.format(i)), final_image)
-
-    elif colour == 'blue':
-        for i in range(number_of_images):
-            # random height and width
-            width = np.random.randint(image_wight_range[0], image_wight_range[1])
-            height = np.random.randint(image_height_range[0], image_height_range[1])
-            # create and save image
-            image_blue = np.random.randint(0, 255, (width, height, 1))
-            image_zeros = np.zeros((width, height, 1), np.uint8)
-            final_image = np.concatenate((image_blue, image_zeros, image_zeros), axis=-1)
+        elif part_number_of_images <= i < (part_number_of_images * 2):
+            final_image = np.concatenate((image, image_zeros, image_zeros), axis=-1)
             cv2.imwrite(os.path.join(DATASET_PATH_BLUE, 'blue_{}.jpg'.format(i)), final_image)
-
-    else:
-        for i in range(number_of_images):
-            # random wight and height
-            width = np.random.randint(image_wight_range[0], image_wight_range[1])
-            height = np.random.randint(image_height_range[0], image_height_range[1])
-            # create and save image
-            image_green = np.random.randint(0, 255, (width, height, 1))
-            image_zeros = np.zeros((width, height, 1), np.uint8)
-            final_image = np.concatenate((image_zeros, image_green, image_zeros), axis=-1)
+        else:
+            final_image = np.concatenate((image_zeros, image, image_zeros), axis=-1)
             cv2.imwrite(os.path.join(DATASET_PATH_GREEN, 'green_{}.jpg'.format(i)), final_image)
 
 
-def make_data_json(test_image: float) -> None:
+def make_data_json(proportion_test_images: float) -> None:
     """
     This function create json file with train and test data of images.
 
-    :param test_image: this parameter is set in the range from 0 to 1.
-    :return: None
+    :param proportion_test_images: this parameter is set in the range from 0 to 1.
     """
-
     # directory list
     red_catalog = os.listdir(DATASET_PATH_RED)
     green_catalog = os.listdir(DATASET_PATH_GREEN)
     blue_catalog = os.listdir(DATASET_PATH_BLUE)
 
     # number of test images
-    len_test_red = int(len(red_catalog) * test_image)
-    len_test_green = int(len(green_catalog) * test_image)
-    len_test_blue = int(len(blue_catalog) * test_image)
+    len_test_red = int(len(red_catalog) * proportion_test_images)
+    len_test_green = int(len(green_catalog) * proportion_test_images)
+    len_test_blue = int(len(blue_catalog) * proportion_test_images)
 
     # create dictionary
     train_test_image_json = {'train': {}, 'test': {}}
 
     # creating a complete dictionary
-    for i in range(len(red_catalog)):
+    for i, img_path in enumerate(red_catalog):
         if i < len_test_red:
-            train_test_image_json['test'][DATASET_PATH_RED + red_catalog[i]] = 'red'
+            train_test_image_json['test'][os.path.join(DATASET_PATH_RED, img_path)] = 'red'
         else:
-            train_test_image_json['train'][DATASET_PATH_RED + red_catalog[i]] = 'red'
+            train_test_image_json['train'][os.path.join(DATASET_PATH_RED, img_path)] = 'red'
 
-    for i in range(len(green_catalog)):
+    for i, img_path in enumerate(green_catalog):
         if i < len_test_green:
-            train_test_image_json['test'][DATASET_PATH_GREEN + green_catalog[i]] = 'green'
+            train_test_image_json['test'][os.path.join(DATASET_PATH_GREEN, img_path)] = 'green'
         else:
-            train_test_image_json['train'][DATASET_PATH_GREEN + green_catalog[i]] = 'green'
+            train_test_image_json['train'][os.path.join(DATASET_PATH_GREEN, img_path)] = 'green'
 
-    for i in range(len(blue_catalog)):
+    for i, img_path in enumerate(blue_catalog):
         if i < len_test_blue:
-            train_test_image_json['test'][DATASET_PATH_BLUE + blue_catalog[i]] = 'blue'
+            train_test_image_json['test'][os.path.join(DATASET_PATH_BLUE, img_path)] = 'blue'
         else:
-            train_test_image_json['train'][DATASET_PATH_BLUE + blue_catalog[i]] = 'blue'
+            train_test_image_json['train'][os.path.join(DATASET_PATH_BLUE, img_path)] = 'blue'
 
     with open(os.path.join('data.json'), 'w') as f:
         json.dump(train_test_image_json, f, indent=4)
 
 
 if __name__ == '__main__':
-
-    create_dataset(number_of_images=100, colour='green', image_wight_range=(300, 500), image_height_range=(300, 500),
-                   create_dataset=True)
-    make_data_json(test_image=0.2)
+    create_dataset(number_of_images=300, image_wight_range=(300, 500), image_height_range=(300, 500))
+    make_data_json(proportion_test_images=0.2)
--- test.py
+++ test.py
@@ -1,16 +1,26 @@
-import math
 from typing import Tuple
+import math
 import json
+import os
+#from math import ceil
+
 import cv2
 import numpy as np
-import random
 import matplotlib.pyplot as plt
-from math import ceil
+
+JSON_FILE_PATH = os.path.abspath('./')
 
 
 class DataGenerator:
     def __init__(self, json_path: str, batch_size: int, is_train: bool, image_shape: Tuple[int, int, int]) -> None:
+        """
+        This function reads parameters, reads json and shuffles the data.
 
+        :param json_path: this is path for json file
+        :param batch_size: number of images in one batch
+        :param is_train: this is bool value, if is_train = True, then we work with train images, otherwise with test.
+        :param image_shape: this is image shape
+        """
         self.json_path = json_path
         self.batch_size = batch_size
         self.is_train = is_train
@@ -18,32 +28,28 @@ class DataGenerator:
         self.num_classes = 3
 
         # read json
-        with open('datasetdata.json') as f:  # открыли файл с данными
-            dicti = json.load(f)
-
+        with open(os.path.join(json_path, 'data.json')) as f:  # открыли файл с данными
+            train_test_image_json = json.load(f)
 
         if is_train:
-            self.data = dicti['train']
+            self.data = train_test_image_json['train']
         else:
-            self.data = dicti['test']
+            self.data = train_test_image_json['test']
 
         # shuffle data
-        """
-        Тут соотвественно перемешиваются наши данные, если сделать print(), то можно увидеть, что перемешалось всё 
-        хорошо. 
-        
-        """
         data = list(self.data.items())
-        random.shuffle(data)
+        np.random.shuffle(data)
         self.data = dict(data)
-        #print(self.data)
+        return
 
-
-    def __iter__(self):
+    def __iter__(self) -> object:
         return self
 
-    def __next__(self):
+    def __next__(self) -> object:
+        """
 
+        :return:
+        """
         self.counter = 0
         if self.counter < len(self):
             self.counter += 1
@@ -51,38 +57,30 @@ class DataGenerator:
         else:
             raise StopIteration
 
-    def __len__(self):
-
+    def __len__(self) -> int:
         """
-        :return:
-        Here I used "math" module to round the "number_of_batch" up.
+        This function counts numbers of batch
+
+        :return: number_of_batch
         """
-
-        self.number_of_batch = math.ceil(len(self.data) / self.batch_size)
-        return self.number_of_batch
-
+        number_of_batch = math.ceil(len(self.data) / self.batch_size)
+        return number_of_batch
 
     def __getitem__(self, batch_idx: int) -> Tuple[np.ndarray, np.ndarray]:
         """
-        :param batch_idx:
-        :return:
-
         Here we return batch (numpy array(batch_size, image_h, image_w, 3). Also we return "onehot encoding", where there
         are batch_size and number of classes.
+        :param batch_idx:
+        :return:
         """
-
-        data = self.data
-        # тут можно не использовтьа sorted? У меня почему-то раюотает только с sordet.
-        data_list = sorted(data.items())[(batch_idx * self.batch_size) : ((batch_idx + 1) * self.batch_size)]
-        # после sorted я снова перемешиваю
-        data_list = random.sample(data_list, len(data_list))
+        data_list = list(self.data.items())[(batch_idx * self.batch_size) : ((batch_idx + 1) * self.batch_size)]
         self.images = np.zeros((self.batch_size, self.image_shape[0], self.image_shape[1], self.image_shape[2]))
         self.labels = np.zeros((self.batch_size, self.num_classes))
-        # возможно тут начинаются проблемы
         for i, (img_path, class_name) in enumerate(data_list):
             img = cv2.imread(img_path)
-            self.resized_image = cv2.resize(img, (self.image_shape[0], self.image_shape[1]))
-            self.images[i, :, :, :] = self.resized_image
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            resized_image = cv2.resize(img, (self.image_shape[0], self.image_shape[1]))
+            self.images[i, :, :, :] = resized_image
 
             if class_name == 'red':
                 self.labels[i, 0] = 1
@@ -92,16 +90,17 @@ class DataGenerator:
             else:
                 self.labels[i, 2] = 1
 
-            print(self.labels)
         return self.images, self.labels
 
-
     def image_grafic(self):
+        """
+        This function shows images with labels
+
+        """
         plt.figure(figsize=(10, 10))
         for i in range(self.batch_size):
             image = self.images[i]
             image = image.astype("uint8")
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             plt.imshow(image)
             if self.labels[i][0] == 1:
                 plt.title("red")
@@ -109,16 +108,10 @@ class DataGenerator:
                 plt.title("green")
             elif self.labels[i][2] == 1:
                 plt.title("blue")
-
             plt.show()
 
+        return
 
 
-
-
-
-
-x = DataGenerator(json_path='/home/andre/pycharm/python_cv_tasks/task_1', batch_size=16, is_train=False, image_shape=(224, 224, 3))
-
-#x.__getitem__(2)
-#x.image_grafic()
+if __name__ == '__main__':
+    x = DataGenerator(json_path=JSON_FILE_PATH, batch_size=16, is_train=False, image_shape=(224, 224, 3))
