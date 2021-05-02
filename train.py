a48b55358081021a1b535595c298e1deb2466c65
андрей китенко <genialniy.kitenko@gmail.com>
2021-05-02 19:59:31 +0300

    added two lines for memory gpu

--- train.py
+++ train.py
@@ -44,4 +44,6 @@ def train(dataset_path_json: str, save_path: str) -> None:
 
 
 if __name__ == '__main__':
+    devices = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(devices[0], True)
     train(JSON_FILE_PATH, LOGS_DIR)
